cd168d295dcef8a3d2d61edaf2a9e147545ef128
Victor Figueroa <32070827+victorfigueroabuilds@users.noreply.github.com>
2020-06-17 22:00:01 -0400

    added hero section

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,17 @@
 */
 !(function($) {
   "use strict";
+  
+  // Hero typed
+  var typed_strings = $(".typed").data('typed-items');
+  typed_strings = typed_strings.split(',')
+  new Typed('.typed', {
+    strings: typed_strings,
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 2000
+  });
 
   // Nav Menu
   $(document).on('click', '.nav-menu a, .mobile-nav a', function(e) {
