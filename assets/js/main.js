bc42aaa777419c699dffaee3967e29f6890f8e71
Victor <>
2021-02-05 22:59:10 -0500

    Updated content and home page layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,18 +7,77 @@
 !(function($) {
   "use strict";
 
-/*
-  // Hero typed
-  var typed_strings = $(".typed").data('typed-items');
-  typed_strings = typed_strings.split(',')
-  new Typed('.typed', {
-    strings: typed_strings,
-    loop: true,
-    typeSpeed: 100,
-    backSpeed: 50,
-    backDelay: 2000
-  });
-*/
+// Typed Text from https://usefulangle.com/post/75/typing-effect-animation-javascript-css
+  // List of sentences
+  var _CONTENT = [
+  "Determined",
+  "Passionate",
+  "Inovative",
+  "Learning Every Day",
+  "a Student",
+  "a Software Engineer",
+  "Victor Figueroa",
+  ];
+
+  // Current sentence being processed
+  var _PART = 0;
+
+  // Character number of the current sentence being processed
+  var _PART_INDEX = 0;
+
+  // Holds the handle returned from setInterval
+  var _INTERVAL_VAL;
+
+  // Element that holds the text
+  var _ELEMENT = document.querySelector("#text");
+
+  // Implements typing effect
+  function Type() {
+    // Get substring with 1 characater added
+    var text =  _CONTENT[_PART].substring(0, _PART_INDEX + 1);
+    _ELEMENT.innerHTML = text;
+    _PART_INDEX++;
+
+    // If full sentence has been displayed then start to delete the sentence after some time
+    if(text === _CONTENT[_PART]) {
+      // Hide the cursor
+
+      clearInterval(_INTERVAL_VAL);
+      setTimeout(function() {
+        _INTERVAL_VAL = setInterval(Delete, 50);
+      }, 1000);
+    }
+  }
+
+  // Implements deleting effect
+  function Delete() {
+    // Get substring with 1 characater deleted
+    var text =  _CONTENT[_PART].substring(0, _PART_INDEX - 1);
+    _ELEMENT.innerHTML = text;
+    _PART_INDEX--;
+
+    // If sentence has been deleted then start to display the next sentence
+    if(text === '') {
+      clearInterval(_INTERVAL_VAL);
+
+      // If current sentence was last then display the first one, else move to the next
+      if(_PART == (_CONTENT.length - 1))
+        _PART = 0;
+      else
+        _PART++;
+
+      _PART_INDEX = 0;
+
+      // Start to display the next sentence after some time
+      setTimeout(function() {
+        _INTERVAL_VAL = setInterval(Type, 100);
+      }, 200);
+    }
+  }
+
+  // Start the typing effect on load
+  _INTERVAL_VAL = setInterval(Type, 100);
+
   // Nav Menu
   $(document).on('click', '.nav-menu a, .mobile-nav a', function(e) {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
@@ -117,24 +176,6 @@
     });
   }, {
     offset: '80%'
-  });
-
-  // Testimonials carousel (uses the Owl Carousel library)
-  $(".testimonials-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    responsive: {
-      0: {
-        items: 1
-      },
-      768: {
-        items: 2
-      },
-      900: {
-        items: 3
-      }
-    }
   });
 
   // Porfolio isotope and filter
